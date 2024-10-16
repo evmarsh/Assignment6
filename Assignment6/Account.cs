917d400074dd479c18b3ce67065db6cef657ffc5
Evan Marsh <evanmarsh2003@gmail.com>
2024-10-16 11:30:16 -0500

    Added account class

---
+++ Assignment6/Account.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment6
+{
+    internal class Account
+    {
+        private string AccountID { get; set; }
+        private string CustomerName { get; set; }
+        private decimal Balance { get; set; }
+
+        public Account(string accountID, string customerName, decimal balance)
+        {
+            this.AccountID = accountID;
+            this.CustomerName = customerName;
+            this.Balance = balance;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            if (amount < 0)
+            {
+                MessageBox.Show("Deposit amount cannot be less than 0.");
+            }
+            else
+            {
+                Balance += amount;
+            }
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount < 0)
+            {
+                MessageBox.Show("Withdrawl amount cannot be less than 0.");
+            }
+            else if (amount > Balance)
+            {
+                MessageBox.Show("Cannot withdraw more than account balance.");
+            }
+            else
+            {
+                Balance -= amount;
+            }
+        }
+
+        public string GetAccountDetails()
+        {
+            return AccountID + " " + CustomerName + " " + Balance;
+        }
+    }
+}
--- Assignment6/Form1.Designer.cs
+++ Assignment6/Form1.Designer.cs
@@ -34,10 +34,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(207, 105);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(276, 129);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(56, 19);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -45,20 +45,21 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(340, 83);
+            this.label1.Location = new System.Drawing.Point(453, 102);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(44, 16);
             this.label1.TabIndex = 1;
             this.label1.Text = "label1";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
