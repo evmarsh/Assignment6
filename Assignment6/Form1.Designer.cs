31fd464144f80b4762c0a614d4a807e94ce10240
ksaienko <ksaienko@BE-1034-019>
2024-10-21 10:49:10 -0500

    ADDED TAB CONTROL

--- Assignment6/Form1.Designer.cs
+++ Assignment6/Form1.Designer.cs
@@ -44,50 +44,58 @@
             this.txtDeposit = new System.Windows.Forms.TextBox();
             this.txtWithdraw = new System.Windows.Forms.TextBox();
             this.txtBalance = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPageAcc = new System.Windows.Forms.TabPage();
+            this.tabPageOpr = new System.Windows.Forms.TabPage();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAccounts)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPageAcc.SuspendLayout();
+            this.tabPageOpr.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnCreateAccount
             // 
-            this.btnCreateAccount.Location = new System.Drawing.Point(306, 64);
+            this.btnCreateAccount.Location = new System.Drawing.Point(287, 14);
             this.btnCreateAccount.Name = "btnCreateAccount";
-            this.btnCreateAccount.Size = new System.Drawing.Size(95, 23);
+            this.btnCreateAccount.Size = new System.Drawing.Size(101, 51);
             this.btnCreateAccount.TabIndex = 0;
             this.btnCreateAccount.Text = "Create account";
             this.btnCreateAccount.UseVisualStyleBackColor = true;
             // 
             // btnLoad
             // 
-            this.btnLoad.Location = new System.Drawing.Point(306, 118);
+            this.btnLoad.Location = new System.Drawing.Point(287, 84);
             this.btnLoad.Name = "btnLoad";
-            this.btnLoad.Size = new System.Drawing.Size(95, 23);
+            this.btnLoad.Size = new System.Drawing.Size(101, 55);
             this.btnLoad.TabIndex = 1;
             this.btnLoad.Text = "Load accounts";
             this.btnLoad.UseVisualStyleBackColor = true;
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(306, 165);
+            this.btnSave.Location = new System.Drawing.Point(422, 50);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(89, 23);
+            this.btnSave.Size = new System.Drawing.Size(101, 54);
             this.btnSave.TabIndex = 2;
             this.btnSave.Text = "Save accounts";
             this.btnSave.UseVisualStyleBackColor = true;
             // 
             // btnDeposit
             // 
-            this.btnDeposit.Location = new System.Drawing.Point(461, 87);
+            this.btnDeposit.Location = new System.Drawing.Point(354, 27);
             this.btnDeposit.Name = "btnDeposit";
-            this.btnDeposit.Size = new System.Drawing.Size(75, 23);
+            this.btnDeposit.Size = new System.Drawing.Size(101, 51);
             this.btnDeposit.TabIndex = 3;
             this.btnDeposit.Text = "Deposit";
             this.btnDeposit.UseVisualStyleBackColor = true;
             // 
             // btnWithdraw
             // 
-            this.btnWithdraw.Location = new System.Drawing.Point(461, 144);
+            this.btnWithdraw.Location = new System.Drawing.Point(354, 112);
             this.btnWithdraw.Name = "btnWithdraw";
-            this.btnWithdraw.Size = new System.Drawing.Size(75, 23);
+            this.btnWithdraw.Size = new System.Drawing.Size(101, 48);
             this.btnWithdraw.TabIndex = 4;
             this.btnWithdraw.Text = "Withdraw";
             this.btnWithdraw.UseVisualStyleBackColor = true;
@@ -95,7 +103,7 @@
             // lblAccId
             // 
             this.lblAccId.AutoSize = true;
-            this.lblAccId.Location = new System.Drawing.Point(30, 67);
+            this.lblAccId.Location = new System.Drawing.Point(6, 33);
             this.lblAccId.Name = "lblAccId";
             this.lblAccId.Size = new System.Drawing.Size(61, 13);
             this.lblAccId.TabIndex = 5;
@@ -104,7 +112,7 @@
             // lblName
             // 
             this.lblName.AutoSize = true;
-            this.lblName.Location = new System.Drawing.Point(30, 108);
+            this.lblName.Location = new System.Drawing.Point(6, 71);
             this.lblName.Name = "lblName";
             this.lblName.Size = new System.Drawing.Size(83, 13);
             this.lblName.TabIndex = 6;
@@ -113,7 +121,7 @@
             // lblDeposit
             // 
             this.lblDeposit.AutoSize = true;
-            this.lblDeposit.Location = new System.Drawing.Point(26, 144);
+            this.lblDeposit.Location = new System.Drawing.Point(35, 46);
             this.lblDeposit.Name = "lblDeposit";
             this.lblDeposit.Size = new System.Drawing.Size(46, 13);
             this.lblDeposit.TabIndex = 7;
@@ -122,7 +130,7 @@
             // lblWithdraw
             // 
             this.lblWithdraw.AutoSize = true;
-            this.lblWithdraw.Location = new System.Drawing.Point(25, 175);
+            this.lblWithdraw.Location = new System.Drawing.Point(35, 81);
             this.lblWithdraw.Name = "lblWithdraw";
             this.lblWithdraw.Size = new System.Drawing.Size(55, 13);
             this.lblWithdraw.TabIndex = 8;
@@ -131,7 +139,7 @@
             // lblBalance
             // 
             this.lblBalance.AutoSize = true;
-            this.lblBalance.Location = new System.Drawing.Point(25, 205);
+            this.lblBalance.Location = new System.Drawing.Point(6, 105);
             this.lblBalance.Name = "lblBalance";
             this.lblBalance.Size = new System.Drawing.Size(49, 13);
             this.lblBalance.TabIndex = 9;
@@ -140,73 +148,127 @@
             // dataGridViewAccounts
             // 
             this.dataGridViewAccounts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewAccounts.Location = new System.Drawing.Point(30, 243);
+            this.dataGridViewAccounts.Location = new System.Drawing.Point(32, 253);
             this.dataGridViewAccounts.Name = "dataGridViewAccounts";
-            this.dataGridViewAccounts.Size = new System.Drawing.Size(532, 181);
+            this.dataGridViewAccounts.Size = new System.Drawing.Size(552, 207);
             this.dataGridViewAccounts.TabIndex = 10;
             // 
             // txtAccountId
             // 
-            this.txtAccountId.Location = new System.Drawing.Point(120, 67);
+            this.txtAccountId.Location = new System.Drawing.Point(95, 26);
             this.txtAccountId.Name = "txtAccountId";
-            this.txtAccountId.Size = new System.Drawing.Size(100, 20);
+            this.txtAccountId.Size = new System.Drawing.Size(154, 20);
             this.txtAccountId.TabIndex = 11;
             // 
             // txtName
             // 
-            this.txtName.Location = new System.Drawing.Point(119, 105);
+            this.txtName.Location = new System.Drawing.Point(95, 64);
             this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.Size = new System.Drawing.Size(154, 20);
             this.txtName.TabIndex = 12;
             // 
             // txtDeposit
             // 
-            this.txtDeposit.Location = new System.Drawing.Point(120, 144);
+            this.txtDeposit.Location = new System.Drawing.Point(114, 43);
             this.txtDeposit.Name = "txtDeposit";
-            this.txtDeposit.Size = new System.Drawing.Size(100, 20);
+            this.txtDeposit.Size = new System.Drawing.Size(154, 20);
             this.txtDeposit.TabIndex = 13;
             // 
             // txtWithdraw
             // 
-            this.txtWithdraw.Location = new System.Drawing.Point(120, 175);
+            this.txtWithdraw.Location = new System.Drawing.Point(114, 78);
             this.txtWithdraw.Name = "txtWithdraw";
-            this.txtWithdraw.Size = new System.Drawing.Size(100, 20);
+            this.txtWithdraw.Size = new System.Drawing.Size(154, 20);
             this.txtWithdraw.TabIndex = 14;
             // 
             // txtBalance
             // 
-            this.txtBalance.Location = new System.Drawing.Point(120, 202);
+            this.txtBalance.Location = new System.Drawing.Point(94, 98);
             this.txtBalance.Name = "txtBalance";
-            this.txtBalance.Size = new System.Drawing.Size(100, 20);
+            this.txtBalance.Size = new System.Drawing.Size(154, 20);
             this.txtBalance.TabIndex = 15;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPageAcc);
+            this.tabControl1.Controls.Add(this.tabPageOpr);
+            this.tabControl1.Location = new System.Drawing.Point(28, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(560, 216);
+            this.tabControl1.TabIndex = 16;
+            // 
+            // tabPageAcc
+            // 
+            this.tabPageAcc.Controls.Add(this.lblAccId);
+            this.tabPageAcc.Controls.Add(this.txtBalance);
+            this.tabPageAcc.Controls.Add(this.btnCreateAccount);
+            this.tabPageAcc.Controls.Add(this.btnLoad);
+            this.tabPageAcc.Controls.Add(this.btnSave);
+            this.tabPageAcc.Controls.Add(this.txtName);
+            this.tabPageAcc.Controls.Add(this.txtAccountId);
+            this.tabPageAcc.Controls.Add(this.lblName);
+            this.tabPageAcc.Controls.Add(this.lblBalance);
+            this.tabPageAcc.Location = new System.Drawing.Point(4, 22);
+            this.tabPageAcc.Name = "tabPageAcc";
+            this.tabPageAcc.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageAcc.Size = new System.Drawing.Size(552, 190);
+            this.tabPageAcc.TabIndex = 0;
+            this.tabPageAcc.Text = "Account details";
+            this.tabPageAcc.UseVisualStyleBackColor = true;
+            // 
+            // tabPageOpr
+            // 
+            this.tabPageOpr.Controls.Add(this.textBox1);
+            this.tabPageOpr.Controls.Add(this.btnDeposit);
+            this.tabPageOpr.Controls.Add(this.txtWithdraw);
+            this.tabPageOpr.Controls.Add(this.label1);
+            this.tabPageOpr.Controls.Add(this.lblWithdraw);
+            this.tabPageOpr.Controls.Add(this.btnWithdraw);
+            this.tabPageOpr.Controls.Add(this.lblDeposit);
+            this.tabPageOpr.Controls.Add(this.txtDeposit);
+            this.tabPageOpr.Location = new System.Drawing.Point(4, 22);
+            this.tabPageOpr.Name = "tabPageOpr";
+            this.tabPageOpr.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageOpr.Size = new System.Drawing.Size(552, 190);
+            this.tabPageOpr.TabIndex = 1;
+            this.tabPageOpr.Text = "Operations";
+            this.tabPageOpr.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(114, 112);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(154, 20);
+            this.textBox1.TabIndex = 18;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Balance:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 447);
-            this.Controls.Add(this.txtBalance);
-            this.Controls.Add(this.txtWithdraw);
-            this.Controls.Add(this.txtDeposit);
-            this.Controls.Add(this.txtName);
-            this.Controls.Add(this.txtAccountId);
+            this.ClientSize = new System.Drawing.Size(600, 499);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.dataGridViewAccounts);
-            this.Controls.Add(this.lblBalance);
-            this.Controls.Add(this.lblWithdraw);
-            this.Controls.Add(this.lblDeposit);
-            this.Controls.Add(this.lblName);
-            this.Controls.Add(this.lblAccId);
-            this.Controls.Add(this.btnWithdraw);
-            this.Controls.Add(this.btnDeposit);
-            this.Controls.Add(this.btnSave);
-            this.Controls.Add(this.btnLoad);
-            this.Controls.Add(this.btnCreateAccount);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "BANK ACCOUNTS";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAccounts)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPageAcc.ResumeLayout(false);
+            this.tabPageAcc.PerformLayout();
+            this.tabPageOpr.ResumeLayout(false);
+            this.tabPageOpr.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -228,6 +290,11 @@
         private System.Windows.Forms.TextBox txtDeposit;
         private System.Windows.Forms.TextBox txtWithdraw;
         private System.Windows.Forms.TextBox txtBalance;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPageAcc;
+        private System.Windows.Forms.TabPage tabPageOpr;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
